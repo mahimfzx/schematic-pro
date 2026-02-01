ef619f2fad636f5f01936bb79228b91e8bbd77cb
mahimfzx <mahimfzx@gmail.com>
2026-02-02 00:15:31 +0600

    Implement service worker for caching files
    This service worker caches essential files for offline use.

---
+++ sw.js
@@ -0,0 +1,22 @@
+const cacheName = 'pwa-v1';
+const filesToCache = [
+    '/',
+    '/index.html',
+    '/manifest.json'
+];
+
+self.addEventListener('install', (e) => {
+    e.waitUntil(
+        caches.open(cacheName).then((cache) => {
+            return cache.addAll(filesToCache);
+        })
+    );
+});
+
+self.addEventListener('fetch', (e) => {
+    e.respondWith(
+        caches.match(e.request).then((response) => {
+            return response || fetch(e.request);
+        })
+    );
+});
